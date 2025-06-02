92900a7b048fe59d57d382f9dde9d9bde9c4eb90
SilverLuke <luca99.argentieri@gmail.com>
2025-06-02 15:47:46 +0200

    Adding Motor_PID library

---
+++ robot/lib/Motor_PID/src/Motor_PID.cpp
@@ -0,0 +1,226 @@
+#include "Motor_PID.h"
+
+#include <Encoder.h>
+#include <sys/_intsup.h>
+
+Motor::Motor(Encoder* encoder, const uint8_t in1, const uint8_t in2,
+             const uint8_t pwmPin, const uint8_t lowerLimit, const uint8_t upperLimit) {
+  this->encoder = encoder;
+  this->in1 = in1;
+  this->in2 = in2;
+  this->pwmpin = pwmPin;
+  this->lower_limit = lowerLimit;
+  this->upper_limit = upperLimit;
+  this->set_position(0);
+
+  // TODO delete this
+  int32_t val = this->encoder->read();
+  if (val != 0) {
+    Serial.println("NOOOOOOO");
+  }
+}
+
+void Motor::init(const double kp, const double ki, const double kd) {
+  this->kp = kp;
+  this->kd = kd;
+  this->ki = ki;
+  if (pwmpin != 0) {
+    pinMode(pwmpin, OUTPUT);
+  }
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  this->direction = FORWARD;
+}
+
+void Motor::config_set_pid(const double kp, const double ki, const double kd) {
+  this->kp = kp;
+  this->kd = kd;
+  this->ki = ki;
+  this->previous_error = 0;
+  this->eintegral = 0;
+  this->previus_timestamp = 0;
+}
+
+void Motor::set_motor(const Direction dir, int pwmVal) const {
+
+  if (pwmpin != 0) {
+    set_motor_with_pwm_pin(dir, pwmVal);
+  } else {
+    set_motor_without_pwm_pin(dir, pwmVal);
+  }
+}
+
+void Motor::set_motor_with_pwm_pin(const Direction dir, int pwmValue) const {
+  analogWrite(pwmpin, pwmValue);
+
+  switch (dir) {
+    case FORWARD:
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      break;
+    case BACKWARD:
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      break;
+    default:
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+      break;
+  }
+}
+
+void Motor::set_motor_without_pwm_pin(const Direction dir,
+                                      const int pwmValue) const {
+  switch (dir) {
+    case FORWARD:
+      analogWrite(in1, pwmValue);
+      analogWrite(in2, LOW);
+      break;
+    case BACKWARD:
+      analogWrite(in1, LOW);
+      analogWrite(in2, pwmValue);
+      break;
+    case BRAKE:
+      analogWrite(in1, LOW);
+      analogWrite(in2, LOW);
+      break;
+    case FREE:
+      analogWrite(in1, LOW);
+      analogWrite(in2, LOW);
+      break;
+    default:
+      break;
+  }
+}
+
+void Motor::loop() {
+  const unsigned long current_time = millis();
+  const long current_position = this->read_encoder();
+
+  // Calculate time delta in seconds
+  const unsigned long delta_time = current_time - previus_timestamp;
+  previus_timestamp = current_time;
+
+  // Calculate PID control signal
+  const double control_signal =
+      calculate_pid_signal(current_position, delta_time);
+
+  // Convert control signal to motor commands
+  this->power = limit_power(std::fabs(control_signal));
+
+  // Update motor state based on position error
+  const long position_error = current_position - target;
+
+  // If the direction is FREE, do nothing
+  if (this->direction == FREE) {
+    this->power = 0;
+    this->set_motor(FREE, this->power);
+    return;
+  }
+
+  if (std::abs(position_error) <= POSITION_TOLERANCE) {
+    this->direction = BRAKE;
+    this->power = 0;
+    this->set_motor(this->direction, this->power);
+    this->target_is_reached = true;
+  } else {
+    this->direction = signbit(control_signal) ? BACKWARD : FORWARD;
+    this->set_motor(direction, this->power);
+    this->target_is_reached = false;
+  }
+}
+
+/**
+ * @brief
+ * @param current_position
+ * @param delta_time Delta time in ms
+ * @return
+ */
+double Motor::calculate_pid_signal(const long current_position,
+                                   const unsigned long delta_time) {
+  const long error = this->target - current_position;
+  const double error_derivative =
+      static_cast<double>(error - this->previous_error) / delta_time;
+  this->previous_error = error;
+
+  this->eintegral = this->eintegral + error * (delta_time / 1000.);
+
+  this->ekp = this->kp * static_cast<double>(error);
+  this->eki = this->ki * this->eintegral;
+  this->ekd = this->kd * error_derivative;
+  return ekp + eki + ekd;
+}
+
+/**
+ * @brief Limit the PID error to the max and min power to the motor
+ * @param power The abs value of the PID error control
+ * @return The power to send to the motor in interval [0; [lower_limit,
+ * upper_limit]]
+ */
+uint8_t Motor::limit_power(const double power) const {
+  if (fabs(power) > 2) {
+    return constrain(power, this->lower_limit, this->upper_limit);
+  }
+  return 0;
+}
+
+long Motor::read_encoder() {
+  this->position = this->encoder->read() / 2;
+  return this->position;
+}
+
+void Motor::turn_on(const Direction dir) {
+  this->direction = dir;
+}
+
+void Motor::turn_off() {
+  this->direction = FREE;
+}
+
+void Motor::set_position(const int32_t pos) {
+  Serial.println("Set position ############################################");
+  this->position = pos;
+  this->encoder->write(this->position * 2);
+}
+
+int32_t Motor::get_position(const bool force_update) {
+  if (force_update)
+    this->read_encoder();
+  return this->position;
+}
+
+void Motor::set_target(const int32_t target) {
+  this->target = target;
+  // PID
+  this->eintegral = 0;
+  this->previous_error = 0;
+  this->previus_timestamp = millis();
+
+  this->target_is_reached = false;
+}
+
+int32_t Motor::get_target() const { return target; }
+
+void Motor::config_set_limit(const int lower_limit, const int upper_limit) {
+  this->lower_limit = lower_limit;
+  this->upper_limit = upper_limit;
+}
+
+bool Motor::target_reached(const bool reset) {
+  if (reset) {
+    this->target_is_reached = false;
+  }
+  return this->target_is_reached;
+}
+
+String Motor::print_state() const {
+  return "Position: " + String(position) + \
+            " Target: " + String(target) + \
+            " Reached: " + String(target_is_reached) + \
+            " Error: " + String(previous_error) + \
+            " Ekp: " + String(ekp) + \
+            " Eki: " + String(eki) + \
+            " Ekd: " + String(ekd) + \
+            " Direction: " + DirectionToString(direction) + \
+            " Power: " + String(power);
+}
---
+++ robot/lib/Motor_PID/src/Motor_PID.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Encoder.h>
+
+enum Direction : uint8_t {
+  FORWARD = 1,
+  BACKWARD = 2,
+  BRAKE = 3,
+  FREE = 4,
+};
+
+inline const char* DirectionToString(Direction dir) {
+  switch (dir) {
+    case FORWARD:
+      return "FORWARD";
+    case BACKWARD:
+      return "BACKWARD";
+    case BRAKE:
+      return "BRAKE";
+    case FREE:
+      return "FREE";
+    default:
+      return "UNKNOWN";
+  }
+}
+
+class Motor {
+ public:
+  Motor() = default;
+  Motor(Encoder* encoder, uint8_t in1, uint8_t in2, uint8_t pwmPin = 0,
+        uint8_t lowerLimit = 50, uint8_t upperLimit = 255);
+  ~Motor() = default;
+
+  /**
+   * @brief place this in the main loop without delays
+   */
+  void loop();
+  /**
+   * @brief Initialize some part of the motor
+   * @param kp[in] Set the KP
+   * @param ki[in] Set the KI
+   * @param kd[in] Set the KD
+   */
+  void init(double kp, double ki, double kd);
+  // changes motor_state variable to true
+  void turn_on(Direction dir = FORWARD);
+  // changes motor_state variable to false
+  void turn_off();
+  void set_position(int32_t pos);
+  int32_t get_position(bool force_update = false);
+  void set_target(int32_t target);
+  [[nodiscard]] int32_t get_target() const;
+  /**
+   * @brief check if target position is reached by motor
+   * @param reset
+   * @return
+   */
+  bool target_reached(bool reset = false);
+  /**
+   * @brief
+   * @param lower_limit Minimal motor power?
+   * @param upper_limit Max motor power?
+   */
+  void config_set_limit(int lower_limit, int upper_limit);
+  void config_set_pid(double kp, double ki, double kd);
+  /**
+   * @brief
+   * @param dir Movement direction
+   * @param pwmVal How fast to move
+   */
+  void set_motor(Direction dir, int pwmVal) const;
+
+  String print_state() const;
+
+ private:
+  static constexpr double MAX_POWER = 255.0;
+  static constexpr long POSITION_TOLERANCE = 2;
+
+  uint8_t in2{}, in1{}, pwmpin = 0;
+  uint8_t upper_limit = 0, lower_limit = 0;
+
+  unsigned long previus_timestamp{0};
+  int32_t target = 0;
+
+  long previous_error = 0;
+  double eintegral = 0;
+  bool target_is_reached = false;
+
+  double kp = 0.0f, kd = 0.0f, ki = 0.0f;  // your pid variables
+  int32_t position = 0;                 // position of rotary encoder \ number of pulses
+  Direction direction = BRAKE;  // this variable let the pwm signal go
+  Encoder* encoder{};
+
+  // Debug variables
+  double ekp = 0, eki = 0, ekd = 0;
+  uint8_t power = 0;
+
+  long read_encoder();
+  double calculate_pid_signal(long current_position, unsigned long delta_time);
+  uint8_t limit_power(double power) const;
+
+  void set_motor_with_pwm_pin(Direction dir, int pwmValue) const;
+  void set_motor_without_pwm_pin(Direction dir, int pwmValue) const;
+};
