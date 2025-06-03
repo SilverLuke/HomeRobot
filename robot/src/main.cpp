c4a3175559bccd6d7448202b6ae3d0611c5a2593
SilverLuke <luca99.argentieri@gmail.com>
2025-06-03 22:46:16 +0200

    Clean up code to basic working robot

--- robot/src/main.cpp
+++ robot/src/main.cpp
@@ -56,105 +56,89 @@ void setup() {
     delay(5000);
   }
 
-  /*
-    init_wifi();
+  init_wifi();
 
-    // Port defaults to 3232
-    ArduinoOTA.setPort(54321);
-    ArduinoOTA.setTimeout(10);
+  // Port defaults to 3232
+  ArduinoOTA.setPort(54321);
+  ArduinoOTA.setTimeout(10000);
 
-    // Hostname defaults to esp3232-[MAC]
-    // ArduinoOTA.setHostname("robot.luca.home.arpa");
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("robot.luca.home.arpa");
 
-    // No authentication by default
-    // ArduinoOTA.setPassword("admin");
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
 
-    // Password can be set with it's md5 value as well
-    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-    ArduinoOTA
-        .onStart([]() {
-          String type;
-          if (ArduinoOTA.getCommand() == U_FLASH) {
-            type = "sketch";
-          } else {  // U_SPIFFS
-            type = "filesystem";
-          }
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+          type = "sketch";
+        } else {  // U_SPIFFS
+          type = "filesystem";
+        }
 
-          // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS
-          // using SPIFFS.end()
-          Serial.println("Start updating " + type);
-        })
-        .onEnd([]() { Serial.println("\nEnd"); })
-        .onProgress([](unsigned int progress, unsigned int total) {
-          Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-        })
-        .onError([](ota_error_t error) {
-          Serial.printf("Error[%u]: ", error);
-          if (error == OTA_AUTH_ERROR) {
-            Serial.println("Auth Failed");
-          } else if (error == OTA_BEGIN_ERROR) {
-            Serial.println("Begin Failed");
-          } else if (error == OTA_CONNECT_ERROR) {
-            Serial.println("Connect Failed");
-          } else if (error == OTA_RECEIVE_ERROR) {
-            Serial.println("Receive Failed");
-          } else if (error == OTA_END_ERROR) {
-            Serial.println("End Failed");
-          }
-        });
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS
+        // using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+          Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+          Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+          Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+          Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+          Serial.println("End Failed");
+        }
+      });
 
-    ArduinoOTA.begin();
+  ArduinoOTA.begin();
 
-    init_server_connection();
-  */
+  init_server_connection();
+
   init_motors();
-  // init_i2c();
-  // lidar = new Lidar();
-  // imu = new IMU();
+  init_i2c();
+  lidar = new Lidar();
+  imu = new IMU();
 
-  //  protocol->AddSensor(lidar);
-  //  protocol->AddSensor(imu);
-
-  motor_sx->set_target(360);
-  motor_dx->set_target(360);
+  //protocol->AddSensor(lidar);
+  //protocol->AddSensor(imu);
 
   led_blink("--", LED_GREEN);
 }
 
 ROBOT_STATE state = IDLE;
 
-
 void loop() {
-  if (millis() > 11000) {
-    motor_sx->turn_off();
-    motor_dx->turn_off();
-    motor_sx->loop();
-    motor_dx->loop();
-    // Logger.debug(MAIN_LOGGER, "SX: %s", motor_sx->print_state().c_str());
-    // Logger.debug(MAIN_LOGGER, "DX: %s", motor_dx->print_state().c_str());
-    // Logger.debug(MAIN_LOGGER, "Motor stop");
-    delay(1000);
-    return;
-  }
   state = IDLE;
-  // show_status();
-  //
+  show_status();
+
   if (Serial.available()) {
     state = serial_commands();
   }
-  //
-  // if (protocol->isConnected()) {
-  //   wifi_commands(protocol, lidar, imu);
-  // }
+
+  if (protocol->isConnected()) {
+    wifi_commands(protocol, lidar, imu);
+  }
   Logger.debug(MAIN_LOGGER, "SX: %s", motor_sx->print_state().c_str());
   Logger.debug(MAIN_LOGGER, "DX: %s", motor_dx->print_state().c_str());
 
   apply_state(state, lidar, imu);
 
-  // protocol->Loop();
-  //
-  // ArduinoOTA.handle();
-  // delay(100);
+  protocol->Loop();
+
+  ArduinoOTA.handle();
+  delay(100);
 }
